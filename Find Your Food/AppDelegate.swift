f8d592341dcb31ce48b6480c7513a0865ccaf234
schott <claireschott10@gmail.com>
2019-07-31 13:56:43 -0700

    message

--- Find Your Food/AppDelegate.swift
+++ Find Your Food/AppDelegate.swift
@@ -8,15 +8,18 @@
 
 import UIKit
 import CoreData
+import YelpAPI
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
+    
     var window: UIWindow?
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+
         return true
     }
 
